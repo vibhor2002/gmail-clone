e62e60182f655db0ee231732ea7db43c4e79609d
Vibhor Grover <grovervibhor21@gmail.com>
2023-01-03 21:14:47 +0530

    Header Done

--- src/Header.js
+++ src/Header.js
@@ -1,9 +1,12 @@
 import React from "react";
 import "./Header.css";
 import MenuIcon from "@mui/icons-material/Menu";
-import SearchIcon from '@mui/icons-material/Search';
-import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
-import { IconButton } from "@mui/material";
+import SearchIcon from "@mui/icons-material/Search";
+import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
+import {Avatar, IconButton } from "@mui/material";
+import AppsIcon from "@mui/icons-material/Apps";
+import NotificationsIcon from "@mui/icons-material/Notifications";
+import HelpOutlineIcon from '@mui/icons-material/HelpOutline';
 
 function Header() {
   return (
@@ -12,14 +15,29 @@ function Header() {
         <IconButton>
           <MenuIcon />
         </IconButton>
-        <img src="https://logos-world.net/wp-content/uploads/2020/11/Gmail-Emblem.png" alt="GMAIL"/>
+        <img
+          src="https://logos-world.net/wp-content/uploads/2020/11/Gmail-Emblem.png"
+          alt="GMAIL"
+        />
       </div>
+
       <div className="header__middle">
         <SearchIcon />
-        <input placeholder="Search Mail" type="text"/>
-        <ArrowDropDownIcon className="header__inputCaret"/>
+        <input placeholder="Search mail" type="text" />
+        <ArrowDropDownIcon className="header__inputCaret" />
       </div>
+
       <div className="header__right">
+        <IconButton>
+          <HelpOutlineIcon/>
+        </IconButton>
+        <IconButton>
+          <AppsIcon />
+        </IconButton>
+        <IconButton>
+          <NotificationsIcon />
+        </IconButton>
+        <Avatar/>
       </div>
     </div>
   );
