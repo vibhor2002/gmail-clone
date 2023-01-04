b5212db1cc8e6eb649b4d793ff94472428ca7fdb
Vibhor Grover <grovervibhor21@gmail.com>
2023-01-04 23:06:30 +0530

    Building the EmailLists

--- src/EmailList.js
+++ src/EmailList.js
@@ -1,12 +1,51 @@
-import React from 'react'
-import './EmailList.css'
+import { Checkbox, IconButton } from "@mui/material";
+import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
+import MoreVertIcon from "@mui/icons-material/MoreVert";
+import RedoIcon from "@mui/icons-material/Redo";
+import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
+import ChevronRightIcon from '@mui/icons-material/ChevronRight';
+import KeyboardHideIcon from '@mui/icons-material/KeyboardHide';
+import SettingsIcon from '@mui/icons-material/Settings';
+import React from "react";
+import "./EmailList.css";
 
 function EmailList() {
   return (
-    <div className='emailList'>
-      
+    <div className="emailList">
+      <div className="emailList__settings">
+        <div className="emailList__settingsLeft">
+          <Checkbox />
+          <IconButton>
+            <ArrowDropDownIcon />
+          </IconButton>
+          <IconButton>
+            <RedoIcon />
+          </IconButton>
+          <IconButton>
+            <MoreVertIcon />
+          </IconButton>
+        </div>
+        <div className="emailList__settingsRight">
+          <IconButton>
+            <ChevronLeftIcon />
+          </IconButton>
+          <IconButton>
+            <ChevronRightIcon />
+          </IconButton>
+          <IconButton>
+            <KeyboardHideIcon />
+          </IconButton>
+          <IconButton>
+            <SettingsIcon />
+          </IconButton>
+        </div>
+      </div>
+
+      <div className="emailList__sections">
+        
+      </div>
     </div>
-  )
+  );
 }
 
-export default EmailList
+export default EmailList;
